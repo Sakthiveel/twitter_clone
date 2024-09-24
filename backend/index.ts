be83c4b16079c1a08ab396accd775a37550237f8
Sakthivel <126545453+Sakthiveel@users.noreply.github.com>
2024-09-24 13:28:57 +0530

    upload image backend handling init

--- backend/index.ts
+++ backend/index.ts
@@ -3,27 +3,79 @@ import { addUser, updateUser } from "./src/database/User";
 import { addPost, updatePost } from "./src/database/Post";
 import { authHandler } from "./src/database/auth";
 import cors from "cors";
+import fileUpload, { UploadedFile } from "express-fileupload";
+import ImageKit from "imagekit";
+import { imageUploader } from "./src/database/utils/utils";
 const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+//Midddle for processing file upload
+app.use(fileUpload());
+
+export const imagekit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY ?? null,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY ?? null,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT ?? null,
+});
+
 const corsOptions = {
   origin: "http://localhost:3000", //(https://your-client-app.com)
 };
 app.use(cors(corsOptions));
+
+// Extend the Express Request interface to include the files property
+interface FileUploadRequest extends Request {
+  files?: { [key: string]: UploadedFile };
+}
 app.get("/", (req: Request, res: Response) => {
   res.send("Server running all good");
 });
-app.post("/v1/addUser", async (req: Request, res: Response) => {
+app.post("/v1/addUser", async (req: FileUploadRequest, res: Response) => {
   try {
     const userInfo = req.body;
     console.log("add user", { userInfo });
+    // Access the uploaded file
+    const { profile_photo, bg_photo }: UploadedFile = req.files;
+    let res_1: string, res_2: string;
+    if (typeof profile_photo === "object") {
+      res_1 = await imageUploader(profile_photo);
+      if (!res_1) {
+        throw Error("Image Upload Failed");
+      }
+    }
+    if (typeof bg_photo === "object") {
+      res_2 = await imageUploader(bg_photo);
+      if (!res_2) {
+        throw Error("Image Upload Failed");
+      }
+    }
+
     await addUser(userInfo);
     res.send({ status: true });
   } catch (err) {
     res.send({ status: false, res: err });
   }
 });
+
+// app.post("/upload", (req: Request, res: Response) => {
+//   // Check if files were uploaded
+//   if (!req.files || Object.keys(req.files).length === 0) {
+//     return res.status(400).send("No files were uploaded.");
+//   }
+
+//   // Access the uploaded file
+//   const file = req.files.myFile as UploadedFile;
+
+//   // Move the file to the desired location
+//   file.mv("/path/to/destination/filename.ext", (err) => {
+//     if (err) {
+//       return res.status(500).send(err);
+//     }
+
+//     res.send("File uploaded successfully");
+//   });
+// });
 
 app.post("/v1/updateUser", authHandler, async (req: Request, res: Response) => {
   try {
---
+++ backend/src/database/utils/utils.ts
@@ -0,0 +1,31 @@
+import { imagekit } from "../../..";
+import { UploadedFile } from "express-fileupload";
+
+export const imageUploader = async (file: UploadedFile): Promise<string> => {
+  try {
+    const uploadRes = await imagekit.upload({
+      file: file.data,
+      fileName: file.name, //required
+      extensions: [
+        {
+          name: "google-auto-tagging",
+          maxTags: 5,
+          minConfidence: 95,
+        },
+      ],
+      transformation: {
+        pre: "l-text,i-Imagekit,fs-50,l-end",
+        post: [
+          {
+            type: "transformation",
+            value: "w-100",
+          },
+        ],
+      },
+    });
+    return uploadRes.url;
+  } catch (err) {
+    console.log("image upload", err);
+    return null;
+  }
+};
