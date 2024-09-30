9a0327245fdfba23308cc492b4013fcc9b1fbada
Sakthivel <126545453+Sakthiveel@users.noreply.github.com>
2024-09-30 16:47:47 +0530

    postcard ui comp created

---
+++ frontend/src/components/UI/ProfileCard.tsx
@@ -0,0 +1,9 @@
+export default function PostCards() {
+  return (
+    <div className="avatar size-10">
+      <div className=" rounded-full">
+        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+      </div>
+    </div>
+  );
+}
