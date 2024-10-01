8b577aa9f4c26ce61e3d79ccaf3c37dc62999651
Sakthivel <126545453+Sakthiveel@users.noreply.github.com>
2024-10-01 12:26:47 +0530

    userprofile ui updates

--- frontend/src/components/UI/ProfileCard.tsx
+++ frontend/src/components/UI/ProfileCard.tsx
@@ -1,8 +1,15 @@
-export default function PostCards() {
+interface PostCardProps {
+  url: string | null;
+}
+export default function PostCards(postCardInfo: PostCardProps) {
+  const { url } = postCardInfo;
+  const urlToUse =
+    url ||
+    "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp";
   return (
     <div className="avatar size-10">
       <div className=" rounded-full">
-        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        <img src={urlToUse} className="object-contain" />
       </div>
     </div>
   );
