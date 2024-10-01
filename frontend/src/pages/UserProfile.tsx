75570501d01f7cc750b5768cdd59044e3e63e7bf
Sakthivel <126545453+Sakthiveel@users.noreply.github.com>
2024-10-01 12:02:48 +0530

    userprofile page ui updates

--- frontend/src/pages/UserProfile.tsx
+++ frontend/src/pages/UserProfile.tsx
@@ -5,9 +5,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { increment } from "../store/userAction";
 import { useParams } from "react-router-dom";
 import { globalLoaderToggle } from "../store/Action";
+import Button from "../components/UI/Button";
 
 export default function UserProfile() {
-  const [userInfo, setUserInfo] = React.useState<User>(() => defaultuser);
+  const auth = useSelector((state) => state.auth);
+  const [userInfo, setUserInfo] = React.useState<User>(() => auth.userInfo);
   const [previewProfilePhot, setPreviewProifilePhoto] = React.useState<
     string | null
   >(null);
@@ -23,7 +25,6 @@ export default function UserProfile() {
   const dispatch = useDispatch();
 
   const imageHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(increment());
     const file = event.target.files?.[0];
     if (file && file.size > 1048576) {
       alert("Image size is greater than 1mb . Please choose another photo");
@@ -45,99 +46,114 @@ export default function UserProfile() {
     }
   };
 
-  const saveHandler = async () => {
-    //edit hadling here
-
+  const editUserInfo = async () => {
+    dispatch(globalLoaderToggle());
     const { error } = userSchema.validate(userInfo);
     if (error) {
       alert(error.message);
       return;
     }
     try {
-      const response = await addUser(userInfo);
+      const response = await editUserInfo(userInfo);
       console.log({ response });
     } catch (err) {
       alert(err);
+    } finally {
+      dispatch(globalLoaderToggle());
     }
   };
 
   return (
-    <div className="flex flex-col w-[500px] gap-2">
-      {previewProfilePhot && (
-        <img src={previewProfilePhot} alt="profile_photo" className="size-40" />
-      )}
-      {previewBgPhoto && (
-        <img src={previewBgPhoto} alt="background-photo" className="size-40" />
-      )}
-      <input
-        type="file"
-        name={UserKeys.bg_photo}
-        accept="image/*"
-        className="file-input file-input-bordered file-input-md w-full max-w-xs"
-        onChange={imageHandler}
-      />
-      <button onClick={() => dispatch(globalLoaderToggle())}>
-        click here{" "}
-      </button>
-      <span>{globalLoader ? "Loading" : "Not Loading"}</span>
-      <input
-        type="file"
-        name={UserKeys.profile_photo}
-        accept="image/*"
-        className="file-input file-input-bordered file-input-md w-full max-w-xs"
-        onChange={imageHandler}
-      />
+    <div className="w-full h-full flex items-center justify-center border">
+      <div className="border w-fit rounded-xl border-grey p-4">
+        <div className="text-xl font-medium mb-2">User Information:</div>
+        <div className="flex flex-col w-[500px] gap-2">
+          {previewProfilePhot && (
+            <img
+              src={previewProfilePhot}
+              alt="profile_photo"
+              className="size-40"
+            />
+          )}
+          {previewBgPhoto && (
+            <img
+              src={previewBgPhoto}
+              alt="background-photo"
+              className="size-40"
+            />
+          )}
+          <input
+            type="file"
+            name={UserKeys.bg_photo}
+            accept="image/*"
+            className="file-input file-input-bordered file-input-md w-full max-w-xs"
+            onChange={imageHandler}
+          />
+          <button onClick={() => dispatch(globalLoaderToggle())}>
+            click here{" "}
+          </button>
+          <span>{globalLoader ? "Loading" : "Not Loading"}</span>
+          <input
+            type="file"
+            name={UserKeys.profile_photo}
+            accept="image/*"
+            className="file-input file-input-bordered file-input-md w-full max-w-xs"
+            onChange={imageHandler}
+          />
 
-      <input
-        type="text"
-        name={UserKeys.display_name}
-        placeholder="Name"
-        className="input input-bordered w-full max-w-xs"
-        value={userInfo.display_name}
-        onChange={(ev) =>
-          setUserInfo((prevSt) =>
-            Object.assign({}, prevSt, { display_name: ev.target.value })
-          )
-        }
-      />
-      <input
-        type="text"
-        name={UserKeys.handler_name}
-        placeholder="handler_name"
-        className="input input-bordered w-full max-w-xs"
-        value={userInfo.handler_name}
-        onChange={(ev) =>
-          setUserInfo((prevSt) =>
-            Object.assign({}, prevSt, { display_name: ev.target.value })
-          )
-        }
-      />
-      <input
-        type="number"
-        name={UserKeys.age}
-        placeholder="age"
-        className="input input-bordered w-full max-w-xs"
-        value={userInfo.age}
-        onChange={(ev) =>
-          setUserInfo((prevSt) =>
-            Object.assign({}, prevSt, { age: ev.target.value })
-          )
-        }
-      />
-      <textarea
-        className="textarea"
-        placeholder="Bio"
-        name={UserKeys.bio}
-        value={userInfo.bio}
-        onChange={(ev) =>
-          setUserInfo((prevSt) =>
-            Object.assign({}, prevSt, { bio: ev.target.value })
-          )
-        }
-      ></textarea>
-      <button className="btn bg-blue-500 text-white" onClick={saveHandler}>
-        Save changes
-      </button>
+          <input
+            type="text"
+            name={UserKeys.display_name}
+            placeholder="Name"
+            className="input input-bordered w-full max-w-xs"
+            value={userInfo.display_name}
+            onChange={(ev) =>
+              setUserInfo((prevSt) =>
+                Object.assign({}, prevSt, { display_name: ev.target.value })
+              )
+            }
+          />
+          <input
+            type="text"
+            name={UserKeys.handler_name}
+            placeholder="handler_name"
+            className="input input-bordered w-full max-w-xs"
+            value={userInfo.handler_name}
+            onChange={(ev) =>
+              setUserInfo((prevSt) =>
+                Object.assign({}, prevSt, { display_name: ev.target.value })
+              )
+            }
+          />
+          <input
+            type="number"
+            name={UserKeys.age}
+            placeholder="age"
+            className="input input-bordered w-full max-w-xs"
+            value={userInfo.age}
+            onChange={(ev) =>
+              setUserInfo((prevSt) =>
+                Object.assign({}, prevSt, { age: ev.target.value })
+              )
+            }
+          />
+          <textarea
+            className="textarea"
+            placeholder="Bio"
+            name={UserKeys.bio}
+            value={userInfo.bio}
+            onChange={(ev) =>
+              setUserInfo((prevSt) =>
+                Object.assign({}, prevSt, { bio: ev.target.value })
+              )
+            }
+          ></textarea>
+          <div className="flex justify-between">
+            <Button btnText="Back" clickHandler={() => {}} />
+            <Button btnText="Edit" clickHandler={editUserInfo} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
