8b2bdb9c7f51b42e78fbe510f2f675af6948900f
Sakthivel <126545453+Sakthiveel@users.noreply.github.com>
2024-10-02 20:41:31 +0530

    Update Profile.tsx

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -4,10 +4,22 @@ import ProfileCard from "../../components/UI/ProfileCard";
 import Button from "../../components/UI/Button";
 import { CalendarDays, CircleCheck, MoveLeft } from "lucide-react";
 import { getUser } from "../../Utils";
-import { UserKeys, User } from "../../Schema/Schema";
+import { UserKeys, User, Post, PostKeys } from "../../Schema/Schema";
+import RightSidebar from "../../components/RightSidebar";
+import PostCards from "../../components/PostCards";
+import { useSelector } from "react-redux";
+
 export default function Profile() {
   const params = useParams();
   const navigate = useNavigate();
+  const {
+    auth,
+    main: {
+      app: {
+        usersPosts: { postsDataArr: userPosts },
+      },
+    },
+  } = useSelector((state) => state);
   const defaultUserInfo: User = {
     [UserKeys.uid]: "demo uid",
     // [UserKeys.handler_name]: "demo handler name",
@@ -21,82 +33,114 @@ export default function Profile() {
       console.log("profile page", { userInfo, query: params.handler_name });
       if (status) {
         console.log("oh my god");
-        setUserInfo(userInfo);
       }
-      // if (!user) {
-      //   alert("Something Went wrong");
-      // }
-      // setUserInfo(user);
     };
     getUserHandler();
   }, []);
-  const profileCardClasses = "size-28 absolute bottom-[30px]";
-  const editBtnClasses = "border-8 border-red-400";
+  console.log("profile", { userInfo, userPosts });
   return (
-    <div className="border w-[570px]">
-      <div
-        className="flex 
-Among the Wildflowers
-@deaflibertarian
-·
-3h
-￼
-It’s not good to be friends with school shooters. Just sayingpx-4 gap-6 items-center"
-      >
-        <MoveLeft
-          className="size-6 hover:cursor-pointer"
-          onClick={() => navigate("/home")}
-        />
-        <div>
-          <div className="text-xl font-bold">
-            {userInfo?.[UserKeys.display_name]}
-          </div>
-          <div className="text-sm text-grey">45 posts</div>
-        </div>
-      </div>
-      <div className="size-44 w-full bg-defaultBC"></div>
-      <div className="p-4 pt-0">
-        <div className="flex w-full h-[100px] justify-between">
-          <div className="relative">
-            <ProfileCard classes={profileCardClasses} />
-          </div>
-          <Button
-            btnText="Edit Profile"
-            btnVariant={"outline"}
-            styles={{ marginTop: 12 }}
-          />
-        </div>
-        <div className="">
-          <div className="flex flex-col">
-            <div className="flex gap-2">
-              <div className="text-xl font-extrabold">
+    <div className="flex">
+      <RightSidebar />
+      <div>
+        <div className="w-[570px]">
+          <div className="flex px-4 gap-6 items-center">
+            <MoveLeft
+              className="size-6 hover:cursor-pointer"
+              onClick={() => navigate("/home")}
+            />
+            <div>
+              <div className="text-xl font-bold">
                 {userInfo?.[UserKeys.display_name]}
               </div>
-              <div className="border rounded-3xl flex py-1 px-2 items-center gap-1">
-                <CircleCheck className="size-5" />
-                <div className="text-sm font-bold border-grey">
-                  Get Verified
+              <div className="text-sm text-grey">45 posts</div>
+            </div>
+          </div>
+          {!userInfo?.[UserKeys.bg_photo] && (
+            <div
+              className={`size-44 w-full bg-[url('${
+                userInfo?.[UserKeys.bg_photo]
+              }')]`}
+            ></div>
+          )}
+          {userInfo?.[UserKeys.bg_photo] && (
+            <img
+              className="size-44 w-full aspect-video object-cover"
+              src={userInfo?.[UserKeys.bg_photo] as string}
+              alt="user_bg_photo"
+            />
+          )}
+          <div className="p-4 pt-0">
+            <div className="flex w-full h-[100px] justify-between">
+              <div className="relative">
+                <ProfileCard
+                  classes={"size-28 absolute bottom-[30px]"}
+                  url={userInfo?.[UserKeys.profile_photo] as string | null}
+                />
+              </div>
+              {auth.userInfo.uid === userInfo.uid && (
+                <Button
+                  clickHandler={() => navigate("/settings/user-info")}
+                  btnText="Edit Profile"
+                  btnVariant={"outline"}
+                  styles={{ marginTop: 12 }}
+                />
+              )}
+            </div>
+            <div className="">
+              <div className="flex flex-col">
+                <div className="flex gap-2">
+                  <div className="text-xl font-extrabold">
+                    {userInfo?.[UserKeys.display_name]}
+                  </div>
+                  <div className="border rounded-3xl flex py-1 px-2 items-center gap-1">
+                    <CircleCheck className="size-5" />
+                    <div className="text-sm font-bold border-grey">
+                      Get Verified
+                    </div>
+                  </div>
+                </div>
+                <div className="text-sm text-grey">
+                  {`@${userInfo?.[UserKeys.handler_name]}`}
+                </div>
+              </div>
+              <div className="flex items-center  gap-1 mt-3">
+                <CalendarDays className="text-grey size-4" />
+                <div className="text-sm text-grey ">Joined February 2023</div>
+              </div>
+              <div className="flex gap-2 mt-1">
+                <div className="text-sm">
+                  64 <span className="text-sm text-grey">Following</span>
+                </div>
+                <div className="text-sm">
+                  85 <span className="text-sm text-grey">Followers</span>
                 </div>
               </div>
             </div>
-            <div className="text-sm text-grey">
-              {`@${userInfo?.[UserKeys.handler_name]}`}
-            </div>
-          </div>
-          <div className="flex items-center  gap-1 mt-3">
-            <CalendarDays className="text-grey size-4" />
-            <div className="text-sm text-grey ">Joined February 2023</div>
-          </div>
-          <div className="flex gap-2 mt-1">
-            <div className="text-sm">
-              64 <span className="text-sm text-grey">Following</span>
-            </div>
-            <div className="text-sm">
-              85 <span className="text-sm text-grey">Followers</span>
-            </div>
           </div>
         </div>
+        <RenderUserPosts userPosts={userPosts} auth={auth} />
       </div>
+    </div>
+  );
+}
+interface RenderUserPosts {
+  userPosts: Array<Post>;
+  auth: object;
+}
+function RenderUserPosts(props: RenderUserPosts) {
+  return (
+    <div>
+      {props.userPosts.map((postInfo) => {
+        if (postInfo[PostKeys.created_by] === props.auth.userInfo.uid) {
+          return (
+            <PostCards
+              postInfo={postInfo}
+              key={`user_poste_${postInfo?.[PostKeys.post_id]}`}
+            />
+          );
+        }
+        return null;
+      })}
     </div>
   );
 }
